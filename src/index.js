5e96e23c91e78a3354e02320fb68aa4151e9d486
BandaySajid <mbandaysajid@gmail.com>
2023-06-28 20:20:46 +0530

    revert

--- src/index.js
+++ src/index.js
@@ -1,13 +1,1 @@
-let Gpt;
-if (typeof require === 'function' && typeof require.resolve === 'function') {
-    // CommonJS module system (Node.js)
-    const GptModule = require('./lib/Gpt.js');
-    Gpt = GptModule.default || GptModule;
-} else {
-    // ECMAScript module system (ESM)
-    import('./lib/Gpt.js').then((GptModule) => {
-        Gpt = GptModule.default || GptModule;
-    });
-}
-
-export default Gpt;
+export {default as Gpt} from './lib/Gpt.js';
