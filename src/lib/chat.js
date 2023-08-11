0c55c43016c1d352b4415afb090f8fb62e37ddb6
BandaySajid <mbandaysajid@gmail.com>
2023-08-11 16:49:56 +0530

    fixed stream issues

--- src/lib/chat.js
+++ src/lib/chat.js
@@ -1,5 +1,5 @@
 import { exec } from 'child_process';
-import { Transform } from 'stream';
+import { Readable, Transform } from 'stream';
 import generateRandomId from '../utils/generateRandomId.js';
 import { getBypassPath, parseHeaders, parseMessage } from '../utils/parser.js';
 let bypassPath = getBypassPath();
@@ -24,53 +24,68 @@ export default function chat({ stream, url, headers, parent_message_id, message 
 
     const cmd = `${bypassPath} -s -X POST ${url} ${parseHeaders(headers)} -d "${JSON.stringify(body).replace(/"/g, '\\"')}"`;
     const curlProcess = exec(cmd, () => {
-    })
+    });
     if (stream) {
         let content = '';
-        const transFormStream = new Transform({
-            transform(chunk, encoding, cb) {
-                let jsonData = chunk.toString().split('data: ');
-                jsonData = jsonData.map(str => str.replace(/\n/g, ''));
+        const readableStream = new Readable({
+            read() { }
+        });
 
-                let validJson = [];
-                jsonData.map((elem) => {
-                    try {
-                        JSON.parse(elem);
-                        validJson.push(elem);
-                    }
-                    catch (err) {
-                    }
-                });
+        curlProcess.stdout.on('data', (chunk) => {
+            let jsonData = chunk.toString().split('data: ');
+            jsonData = jsonData.map(str => str.replace(/\n/g, ''));
 
-                let main;
-                if (validJson.includes('DONE')) {
-                    main = validJson[validJson.length - 2];
+            let validJson = [];
+            jsonData.map((elem) => {
+                try {
+                    JSON.parse(elem);
+                    validJson.push(elem);
                 }
-                else {
-                    main = validJson[validJson.length - 1];
+                catch (err) {
                 }
-                if (validJson.length > 0) {
+            });
+
+
+            let main;
+            if (validJson.includes('DONE')) {
+                main = validJson[validJson.length - 2];
+            }
+            else {
+                main = validJson[validJson.length - 1];
+            }
+            if (validJson.length > 0) {
+                try {
                     const parsed = JSON.parse(main).message.content.parts[0];
                     content += parsed.slice(content.length);
-                    cb(null, JSON.stringify({ status: 'message', message: content }));
+                    readableStream.push(JSON.stringify({ status: 'message', message: content }));
+                }
+                catch (err) {
+
                 }
             }
         });
 
-        curlProcess.stdout.pipe(transFormStream);
-        return transFormStream;
+        curlProcess.stdout.on('end', () => {
+            readableStream.emit('end');
+        })
+
+        curlProcess.stderr.on('data', (err) => {
+            readableStream.emit('error', err);
+        });
+
+        return readableStream;
     }
     else {
-        
+
         return new Promise((resolve, reject) => {
-            
+
             let contentArray = [];
             let nonStreamContent = '';
-            
+
             curlProcess.stdout.on('data', (chunk) => {
                 let jsonData = chunk.toString().split('data: ');
                 jsonData = jsonData.map(str => str.replace(/\n/g, ''));
-                
+
                 let validJson = [];
                 jsonData.map((elem) => {
                     try {
@@ -80,7 +95,8 @@ export default function chat({ stream, url, headers, parent_message_id, message 
                     catch (err) {
                     }
                 });
-                
+
+
                 let main;
                 if (validJson.includes('DONE')) {
                     main = validJson[validJson.length - 2];
@@ -95,11 +111,11 @@ export default function chat({ stream, url, headers, parent_message_id, message 
                         contentArray.push(JSON.stringify({ status: 'message', message: nonStreamContent }));
                     }
                     catch (err) {
-                        
+
                     }
                 }
             });
-            
+
             curlProcess.stdout.on('end', () => {
                 resolve(contentArray[contentArray.length - 1]);
             })
